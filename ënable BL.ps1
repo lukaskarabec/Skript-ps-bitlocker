3bbb0d360dbf2746c65f0b781a86ed47658df747
lukaskarabec <lukaskarabec@gmail.com>
2017-10-05 14:20:25 +0200

    05102017-1420

---
+++ ënable BL.ps1
@@ -0,0 +1,13 @@
+﻿Enable-BitLocker -mountpoint C -EncryptionMethod Aes128 -RecoveryPasswordProtector -SkipHardwareTest
+			
+    <#  do
+      {
+        
+        #Write-Progress -Activity "Sifruji disk C:" -Status 'Stav sifrovani...:' -PercentComplete EncryptionPercentage
+        Start-Sleep -Seconds 1
+      }
+      until ($Volume.VolumeStatus -eq 'FullyEncrypted')#>
+			
+      Write-Progress -Activity "Sifruji disk C" -Status 'Stav sifrovani...:' -Completed
+      
+      #$BitlockerStatus = (Get-BitLockerVolume -MountPoint C).ProtectionStatus
